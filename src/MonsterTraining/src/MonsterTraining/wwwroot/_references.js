0e6f015ab75bc748994b770b650baf2753b29206
James Chambers <james@jameschambers.com>
2016-12-14 17:54:43 -0600

    first commit

---
+++ src/MonsterTraining/src/MonsterTraining/wwwroot/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="js/app.js" />
+/// <reference path="js/site.min.js" />
+/// <reference path="js/vendor/foundation.js" />
+/// <reference path="js/vendor/jquery.js" />
+/// <reference path="js/vendor/what-input.js" />
